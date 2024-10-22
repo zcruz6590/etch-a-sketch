50c8835ed09664283d1a24af73480a593da8ca56
zcruz6590 <zoenoel_cruz@Yahoo.com>
2024-10-22 04:11:25 -0700

    created the 50x50px square object

---
+++ script.js
@@ -0,0 +1,11 @@
+let object = document.createElement("div");
+let container = document.querySelector(".container")
+const rows = 16, cols = 16;
+
+object.style.backgroundColor = "cyan"
+object.style.border = "2px solid black"
+object.style.width = "50px"
+object.style.height = "50px"
+
+container.appendChild(object)
+
